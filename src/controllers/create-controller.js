118cf14349457bb2f768557c022438bf6c675552
GustavoArcoverde <garcoverde3@gmail.com>
2022-06-22 13:20:42 -0300

    cria lógica de mostrar dica aleatoria no tips-controller

--- src/controllers/create-controller.js
+++ src/controllers/create-controller.js
@@ -4,8 +4,8 @@ import {bd} from "../infra/bd.js"
 export const create = (app)=>{
     app.post('/create', (req, res)=>{
         const body = req.body
-        const newTip = new Tip(body.tips)
+        const newTip = new Tip(body.tip)
         bd.tips.push(newTip)
-        res.send({"Tip": newTip})
+        res.send({"Dica": newTip})
     })
 }
--- src/controllers/tips-controller.js
+++ src/controllers/tips-controller.js
@@ -3,6 +3,6 @@ import {bd} from "../infra/bd.js"
 
 export const showTip = (app)=>{
     app.get("/tips", (req, res)=>{
-        res.send(bd.tips[Math.floor(Math.random * bd.tips.length)])
+        res.send(bd.tips[Math.floor(Math.random()*bd.tips.length)])
     })
 }
